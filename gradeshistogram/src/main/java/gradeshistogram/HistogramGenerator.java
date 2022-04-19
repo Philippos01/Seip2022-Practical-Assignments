671ade5f691c21be6e830f102cf26d3a3244a602
Philippos Priovolos <prio_01@hotmail.com>
2022-04-19 23:56:38 +0300

    Add the maven project

---
+++ gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
@@ -0,0 +1,70 @@
+package gradeshistogram;
+import java.io.File;
+import java.util.Scanner;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+public class HistogramGenerator {
+
+	public void generateChart(int[] dataValues) {
+		/*
+		 * The XYSeriesCollection object is a set XYSeries series (dataset) that
+		 * can be visualized in the same chart
+		 */
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		/*
+		 * The XYSeries that are loaded in the dataset. There might be many
+		 * series in one dataset.
+		 */
+		XYSeries data = new XYSeries("random values");
+
+		/*
+		 * Populating the XYSeries data object from the input Integer array
+		 * values.
+		 */
+		for (int i = 0; i < dataValues.length; i++) {
+			data.add(i, dataValues[i]);
+		}
+
+		// add the series to the dataset
+		dataset.addSeries(data);
+
+		boolean legend = false; // do not visualize a legend
+		boolean tooltips = false; // do not visualize tooltips
+		boolean urls = false; // do not visualize urls
+
+		// Declare and initialize a createXYLineChart JFreeChart
+		JFreeChart chart = ChartFactory.createXYLineChart("Chart title", "x_axis title", "y_axis_title", dataset,
+				PlotOrientation.VERTICAL, legend, tooltips, urls);
+
+		/*
+		 * Initialize a frame for visualizing the chart and attach the
+		 * previously created chart.
+		 */
+		ChartFrame frame = new ChartFrame("First", chart);
+		frame.pack();
+		// makes the previously created frame visible
+		frame.setVisible(true);
+	}
+
+
+public static void main(String[]args) {
+	try {
+	File f = new File("C:\\Users\\Philippos\\eclipse-workspace\\seip2022_practical_assignments\\gradeshistogram\\target\\classes\\grades.txt");
+	Scanner scanner = new Scanner(f);
+	HistogramGenerator gen = new HistogramGenerator();
+	int [] numbers = new int [(int) f.length()];
+	int i = 0;
+	while(scanner.hasNextInt())
+	{
+	     numbers[i++] = scanner.nextInt();
+	}
+	gen.generateChart(numbers);
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+}// end of main
+}
