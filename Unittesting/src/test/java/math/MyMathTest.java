16a5781d96babf29ea41b9205c328d6332ea93b5
Philippos Priovolos <prio_01@hotmail.com>
2022-06-02 13:38:40 +0300

    #7 Test all normal/border/zero/exception cases

--- Unittesting/src/test/java/math/MyMathTest.java
+++ Unittesting/src/test/java/math/MyMathTest.java
@@ -9,29 +9,59 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 public class MyMathTest {
-    MyMath math=new MyMath();
-    @Test(expected = IllegalArgumentException.class)
-    public void test_factorial_under_0(){math.factorial(-1);}
+    MyMath math = new MyMath();
 
     @Test(expected = IllegalArgumentException.class)
-    public void test_factorial_over_12(){math.factorial(13);}
+    public void test_factorial_under_0() {
+        math.factorial(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_factorial_over_12() {
+        math.factorial(13);
+    }
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void test_is_prime_exception(){
+    public void test_is_prime_exception_negative() {
         thrown.expect(IllegalArgumentException.class);
         thrown.expectMessage("Prime numbers are greater than 1");
         math.isPrime(-1);
     }
 
     @Test
-    public void test_is_prime(){
+    public void test_is_prime_exception_zero() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Prime numbers are greater than 1");
+        math.isPrime(0);
+    }
+
+    @Test
+    public void test_is_prime_exception_one() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Prime numbers are greater than 1");
+        math.isPrime(1);
+    }
+
+
+    @Test
+    public void test_is_prime() {
         Assert.assertEquals(true, math.isPrime(3));
     }
+
     @Test
-    public void test_is_not_prime(){
+    public void test_is_not_prime() {
         Assert.assertEquals(false, math.isPrime(6));
     }
+
+
+    @Test
+    public void test_is_prime_2() {
+        Assert.assertEquals(true, math.isPrime(2));
+    }
 }
+
+
+
